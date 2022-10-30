af20b2a3304e04d076ae7d8076b2f4d133d4b51c
Devika <89661349+Devikaiii@users.noreply.github.com>
2022-10-30 10:00:20 +0530

    Update caption.py

--- plugins/caption.py
+++ plugins/caption.py
@@ -4,7 +4,7 @@ from helper.database import find, addcaption, delcaption
 @Client.on_message(filters.private & filters.command('set_caption'))
 async def add_caption(client, message):
     if len(message.command) == 1:
-       return await message.reply_text("**Give me a caption to set.\n\nExample:- `/set_caption 📕 File Name: {filename}\n\n💾 Size: {filesize}\n\n⏰ Duration: {duration}`**")
+       return await message.reply_text("Invalid Input.")
     caption = message.text.split(" ", 1)[1]
     addcaption(int(message.chat.id), caption)
     await message.reply_text("**Your Caption successfully added ✅**")
@@ -17,7 +17,7 @@ async def delete_caption(client, message):
     delcaption(int(message.chat.id))
     await message.reply_text("**Your caption successfully deleted ✅**")
                                        
-@Client.on_message(filters.private & filters.command('see_caption'))
+@Client.on_message(filters.private & filters.command('view_caption'))
 async def see_caption(client, message): 
     caption = find(int(message.chat.id))[1]
     if caption:
