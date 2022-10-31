4a65ed51469e9db010c9a7b128548c3bfd40916d
Devika <89661349+Devikaiii@users.noreply.github.com>
2022-10-31 20:47:45 +0530

    Update thumbfuc.py

--- plugins/thumbfuc.py
+++ plugins/thumbfuc.py
@@ -14,7 +14,7 @@ async def viewthumb(client,message):
 @Client.on_message(filters.private & filters.command(['delthumb']))
 async def removethumb(client,message):
     delthumb(int(message.chat.id))
-    await message.reply_text("ᴛʜᴜᴍʙɴᴀɪʟ ᴅᴇʟᴇᴛᴇᴅ."),reply_to_message_id=message.message_id
+    await message.reply_text("ᴛʜᴜᴍʙɴᴀɪʟ ᴅᴇʟᴇᴛᴇᴅ.")
 	
 @Client.on_message(filters.private & filters.photo)
 async def addthumbs(client,message):
