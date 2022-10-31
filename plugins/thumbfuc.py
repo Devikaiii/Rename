2c5c09ddaadf1fe8cfa63ca1dd0ec872e21bbe41
Devika <89661349+Devikaiii@users.noreply.github.com>
2022-10-31 21:59:06 +0530

    Update thumbfuc.py

--- plugins/thumbfuc.py
+++ plugins/thumbfuc.py
@@ -9,17 +9,17 @@ async def viewthumb(client,message):
 	   chat_id=message.chat.id, 
 	   photo=thumb)
     else:
-        text = "ꜱᴇᴛ ᴛʜᴜᴍʙɴᴀɪʟ ꜰɪʀꜱᴛ."
+        text = "`No Saved Thumbnail Found.`"
         await message.reply_text(text=text, reply_to_message_id=message.id) 
 		
 @Client.on_message(filters.private & filters.command(['delthumb']))
 async def removethumb(client,message):
     delthumb(int(message.chat.id))
-    await message.reply_text("ᴛʜᴜᴍʙɴᴀɪʟ ᴅᴇʟᴇᴛᴇᴅ.")
+    await message.reply_text(reply_to_message_id=message.id, "ᴛʜᴜᴍʙɴᴀɪʟ ᴅᴇʟᴇᴛᴇᴅ.")
 	
 @Client.on_message(filters.private & filters.photo)
 async def addthumbs(client,message):
     file_id = str(message.photo.file_id)
     addthumb(message.chat.id , file_id)
-    await message.reply_text("**ᴄᴜꜱᴛᴏᴍ ᴛʜᴜᴍʙɴᴀɪʟ ꜱᴀᴠᴇᴅ. ✅️**\n\n__ᴛʜɪꜱ ᴛʜᴜᴍʙɴᴀɪʟ ᴡɪʟʟ ʙᴇ ᴩᴇʀᴍᴇɴᴀɴᴛ ꜰᴏʀ ᴀʟʟ ꜰᴜᴛᴜʀᴇ ᴜᴩʟᴏᴀᴅꜱ.__")
+    await message.reply_text(reply_to_message_id=message.id, "**ᴄᴜꜱᴛᴏᴍ ᴛʜᴜᴍʙɴᴀɪʟ ꜱᴀᴠᴇᴅ. ✅️**\n\n__ᴛʜɪꜱ ᴛʜᴜᴍʙɴᴀɪʟ ᴡɪʟʟ ʙᴇ ᴩᴇʀᴍᴇɴᴀɴᴛ ꜰᴏʀ ᴀʟʟ ꜰᴜᴛᴜʀᴇ ᴜᴩʟᴏᴀᴅꜱ.__")
 	
