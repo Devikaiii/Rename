ca919e7c786c6c8c64333a24ed36359cde776a84
Devika <89661349+Devikaiii@users.noreply.github.com>
2022-10-31 22:07:25 +0530

    Update thumbfuc.py

--- plugins/thumbfuc.py
+++ plugins/thumbfuc.py
@@ -15,11 +15,13 @@ async def viewthumb(client,message):
 @Client.on_message(filters.private & filters.command(['delthumb']))
 async def removethumb(client,message):
     delthumb(int(message.chat.id))
-    await message.reply_text(reply_to_message_id=message.id, "ᴛʜᴜᴍʙɴᴀɪʟ ᴅᴇʟᴇᴛᴇᴅ.")
+    text = "`Thumbnail Deleted Successfully.`"
+    await message.reply_text(text=text, reply_to_message_id=message.id)
 	
 @Client.on_message(filters.private & filters.photo)
 async def addthumbs(client,message):
     file_id = str(message.photo.file_id)
     addthumb(message.chat.id , file_id)
-    await message.reply_text(reply_to_message_id=message.id, "**ᴄᴜꜱᴛᴏᴍ ᴛʜᴜᴍʙɴᴀɪʟ ꜱᴀᴠᴇᴅ. ✅️**\n\n__ᴛʜɪꜱ ᴛʜᴜᴍʙɴᴀɪʟ ᴡɪʟʟ ʙᴇ ᴩᴇʀᴍᴇɴᴀɴᴛ ꜰᴏʀ ᴀʟʟ ꜰᴜᴛᴜʀᴇ ᴜᴩʟᴏᴀᴅꜱ.__")
+    text = "**ᴄᴜꜱᴛᴏᴍ ᴛʜᴜᴍʙɴᴀɪʟ ꜱᴀᴠᴇᴅ. ✅️**\n\n__ᴛʜɪꜱ ᴛʜᴜᴍʙɴᴀɪʟ ᴡɪʟʟ ʙᴇ ᴩᴇʀᴍᴇɴᴀɴᴛ ꜰᴏʀ ᴀʟʟ ꜰᴜᴛᴜʀᴇ ᴜᴩʟᴏᴀᴅꜱ.__"
+    await message.reply_text(text=text, reply_to_message_id=message.id,)
 	
